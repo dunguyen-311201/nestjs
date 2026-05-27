5f95edc5b619c225cf72b4bcb5ce5121bc18832e
Du Nguyen <du.nguyen@asnet.com.vn>
2026-05-27 17:18:15 +0700

    feat: implement Users module with controller, service, and tests

---
+++ user-management-api/src/users/users.controller.ts
@@ -0,0 +1,40 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+
+@Controller('users')
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Post()
+  create(@Body() createUserDto: any) {
+    return this.usersService.create(createUserDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.usersService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.usersService.findOne(+id);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() updateUserDto: any) {
+    return this.usersService.update(+id, updateUserDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.usersService.remove(+id);
+  }
+}
---
+++ user-management-api/src/users/users.service.ts
@@ -0,0 +1,42 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+
+@Injectable()
+export class UsersService {
+  private readonly users: any[] = [];
+
+  create(createUserDto: any) {
+    const newUser = { ...createUserDto, id: Date.now() };
+    this.users.push(newUser);
+    return newUser;
+  }
+
+  findOne(id: number) {
+    const user = this.users.find((user) => user.id === id);
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    return user;
+  }
+
+  findAll() {
+    return this.users;
+  }
+
+  update(id: number, updateUserDto: any) {
+    const user = this.findOne(id);
+    if (user) {
+      Object.assign(user, updateUserDto);
+      return user;
+    }
+    throw new NotFoundException('User not found');
+  }
+
+  remove(arg0: number) {
+    const index = this.users.findIndex((user) => user.id === arg0);
+    if (index !== -1) {
+      const removedUser = this.users.splice(index, 1);
+      return removedUser[0];
+    }
+    throw new NotFoundException('User not found');
+  }
+}
