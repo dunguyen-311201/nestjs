67e1b87914b25b01c7889668df2a98c0f4030730
Du Nguyen <du.nguyen@asnet.com.vn>
2026-05-29 17:41:46 +0700

    feat: implement LoggingMiddleware in UsersModule for request logging

--- user-management-api/src/users/users.module.ts
+++ user-management-api/src/users/users.module.ts
@@ -1,9 +1,16 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
+import { LoggingMiddleware } from './middleware/logging.middleware';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService],
 })
-export class UsersModule {}
+export class UsersModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): void {
+    consumer
+      .apply(LoggingMiddleware)
+      .forRoutes({ path: 'users', method: RequestMethod.ALL });
+  }
+}
