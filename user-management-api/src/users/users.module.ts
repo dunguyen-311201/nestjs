26a73ff69df9b869928b856f171081d11c4cd57a
Du Nguyen <du.nguyen@asnet.com.vn>
2026-06-02 14:06:04 +0700

    feat: configure TypeOrmModule with SQLite for UsersModule and fix controller spec mocking

--- user-management-api/src/users/users.module.ts
+++ user-management-api/src/users/users.module.ts
@@ -1,9 +1,12 @@
 import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { LoggingMiddleware } from './middleware/logging.middleware';
+import { User } from './entities/user.entity';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
   providers: [UsersService],
 })
